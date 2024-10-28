d9fec06426e999f431bc74894ecf82cf43b8cce7
deji <shittuwasiuayodeji@gmail.com>
2024-10-29 00:12:42 +0100

    add Grid section

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,16 @@
+import Grid from "@/components/Grid";
 import Hero from "@/components/Hero";
 import { FloatingNav } from "@/components/ui/FloatingNavbar";
 import { navItems } from "@/data";
 
 export default function Home() {
   return (
-    <div className="relative bg-black">
-      <FloatingNav navItems={navItems} />
-      <Hero />
-    </div>
+    <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
+      <div className="max-w-7xl w-full">
+        <FloatingNav navItems={navItems} />
+        <Hero />
+        <Grid />
+      </div>
+    </main>
   );
 }

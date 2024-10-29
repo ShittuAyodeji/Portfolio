2c7ad2f7d0a44e61ea3fa9d16ffc2b7db2c701eb
deji <shittuwasiuayodeji@gmail.com>
2024-10-29 18:44:41 +0100

    add Footer section

--- data/index.ts
+++ data/index.ts
@@ -206,12 +206,12 @@ export const socialMedia = [
     id: 1,
     img: "/git.svg",
   },
-  {
-    id: 2,
-    img: "/twit.svg",
-  },
-  {
-    id: 3,
-    img: "/link.svg",
-  },
+  // {
+  //   id: 2,
+  //   img: "/twit.svg",
+  // },
+  // {
+  //   id: 3,
+  //   img: "/link.svg",
+  // },
 ];
